ba56ef0b630c6649cc7d1f3ebe48614ae514e91a
Radu91 <rbumbea@pentalog.com>
2019-08-20 16:23:54 +0300

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,19 @@
+const request = require('request');
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'https://api.darksky.net/forecast/a2a3c748804d5995349d1ce445dc77e7/' + latitude + ',' + longitude + '?units=si';
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service', undefined);
+        } else if (body.error) {
+            callback('Unable to find location', undefined);
+        } else {
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out ' +
+                body.currently.precipProbability + '% chance of rain.'
+            );
+        }
+    });
+};
+
+module.exports = forecast;
